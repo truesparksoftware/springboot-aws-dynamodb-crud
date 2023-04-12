9d34e57dc0850bea19804bf29ba1a23da0844243
truesparksoftware@gmail.com <jeevanbarguje1@gmail.com>
2023-04-12 08:17:29 +0530

    added code for dynamodb operation

---
+++ springboot-aws-dynamodb-crud/src/main/java/com/hsbc/controller/CustomerController.java
@@ -0,0 +1,34 @@
+package com.hsbc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.hsbc.model.Customer;
+import com.hsbc.repository.CustomerRepository;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @PostMapping("/add/customer")
+    public Customer saveCustomer(@RequestBody Customer customer) {
+        return customerRepository.saveCustomer(customer);
+    }
+
+    @GetMapping("/get/customer/{id}")
+    public Customer getCustomerById(@PathVariable("id") String customerId) {
+        return customerRepository.getCustomerById(customerId);
+    }
+
+    @DeleteMapping("/delete/customer/{id}")
+    public String deleteCustomerById(@PathVariable("id") String customerId) {
+        return  customerRepository.deleteCustomerById(customerId);
+    }
+
+    @PutMapping("/update/customer/{id}")
+    public String updateCustomer(@PathVariable("id") String customerId, @RequestBody Customer customer) {
+        return customerRepository.updateCustomer(customerId,customer);
+    }
+}
